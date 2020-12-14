8430dadfab3ebee0319cfb158f1dcbce339c2cc2
Ady Ngom <ady.touba.ngom@gmail.com>
2020-12-14 03:02:55 -0500

    final draft and push to netlify

--- src/components/flip-card.js
+++ src/components/flip-card.js
@@ -2,12 +2,24 @@ import React, { Fragment } from "react";
 import "./flip-card.css";
 
 export const FlipCard = ({ type, children }) => {
+  const bgStyle = {
+    backgroundImage: `url("/memory/images/${type}.png")`,
+    backgroundSize: "80%",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+  };
+
+  const frontStyle = {
+    ...bgStyle,
+    backgroundImage: `url("/memory/images/cardinal-games.png")`,
+  };
+
   return (
     <Fragment>
       <div className="flip-container">
         <div className="card flipper" data-type={type}>
-          <div className="front">front</div>
-          <div className="back">{children}</div>
+          <div className="front" style={frontStyle}></div>
+          <div className="back" style={bgStyle}></div>
         </div>
       </div>
     </Fragment>
--- src/memory/Memory.js
+++ src/memory/Memory.js
@@ -1,16 +1,37 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect, useRef } from "react";
 import "./memory.css";
 import { MemoryGame } from "./memory-game";
 import { FlipCard } from "../components";
 
-const Memory = ({ children, level = 2 }) => {
+const Memory = ({ children, level = 1 }) => {
   const [gameDeck, setGameDeck] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [matchesStack, setMatchesStack] = useState([]);
+
+  const refContainer = useRef();
 
   const getGameDeck = async () => {
-    const newGameDeck = await MemoryGame.init();
+    const newGameDeck = await MemoryGame.init(level);
     setGameDeck([...newGameDeck]);
-    console.log(gameDeck);
+  };
+
+  const addCardToStack = (elem) => {
+    setMatchesStack([...matchesStack, elem]);
+  };
+
+  const checkCardsMatch = (container) => {
+    const [card_one, card_two] = matchesStack;
+    setTimeout(() => {
+      if (card_one.dataset.type === card_two.dataset.type) {
+        card_one.classList.add("freeze", "flipped");
+        card_two.classList.add("freeze", "flipped");
+      } else {
+        card_one.classList.remove("flip");
+        card_two.classList.remove("flip");
+      }
+      setMatchesStack([]);
+      container.classList.remove("freeze");
+    }, 1000);
   };
 
   const flipStack = (evt) => {
@@ -18,25 +39,41 @@ const Memory = ({ children, level = 2 }) => {
     const elem = evt.target;
     if (elem.classList.contains("card")) {
       elem.classList.toggle("flip");
-      console.log(elem);
+      addCardToStack(elem);
     }
   };
 
   useEffect(() => {
     getGameDeck();
-    if (gameDeck.length > 0) setIsLoading((t) => !t);
+    setIsLoading((t) => !t);
   }, []);
+
+  useEffect(() => {
+    const { current: container } = refContainer;
+    if (matchesStack.length === 2) {
+      container.classList.add("freeze");
+      checkCardsMatch(container);
+    }
+  }, [matchesStack.length]);
 
   return (
     <Fragment>
-      <div id="memory" onClick={flipStack}>
-        {gameDeck.map((face, idx) => {
-          return (
-            <FlipCard key={idx} type={face}>
-              <span>{face}</span>
-            </FlipCard>
-          );
-        })}
+      <div id="memory" onClick={flipStack} ref={refContainer}>
+        <header>
+          <h1>Memory Game</h1>
+        </header>
+        <div className="wrapper">
+          {gameDeck.map((face, idx) => {
+            return (
+              <FlipCard key={idx} type={face}>
+                <span>{face}</span>
+              </FlipCard>
+            );
+          })}
+        </div>
+        <footer>
+          <small>Click refresh to start a new game</small>
+        </footer>
       </div>
     </Fragment>
   );
